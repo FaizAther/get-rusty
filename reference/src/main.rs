d890ffa6452f69b2c32693917f63babc08789968
FaizAther <faizather@protonmail.com>
2021-11-21 16:09:00 +1000

    bkup

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,3 +1,21 @@
+// fn main() {
+//     let s1 = String::from("hello");
+//     let (s2, len) = calc_len(s1);
+//     println!("{}, {}", s2, len);
+// }
+
+// fn calc_len(s: String) -> (String, usize) {
+//     let len = s.len();
+//     (s, len)
+// }
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let (s2, len) = calc_len(&s1);
+    println!("{}, {}", s2, len);
+}
+
+fn calc_len(s: &String) -> (&String, usize) {
+    // s.push_str("ops");
+    let len = s.len();
+    (s, len)
 }
