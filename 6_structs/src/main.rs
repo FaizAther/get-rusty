3e24113d18b54b723293d37626eaf5a6e07bd80c
FaizAther <faizather@protonmail.com>
2021-11-22 18:48:25 +1000

    more struct

--- 6_structs/src/main.rs
+++ 6_structs/src/main.rs
@@ -56,9 +56,29 @@ fn user_runner() {
     println!("id: {}", id);
 }
 
+#[derive(Debug)]
 struct Rect {
     width: u32,
     height: u32
+}
+
+impl Rect {
+    fn area(&self) -> u64 {
+        return (self.width * self.height) as u64;
+    }
+
+    fn can_hold(&self, other: &Rect) -> bool {
+        return self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rect {
+    fn square(size: u64) -> Rect {
+        return Rect {
+            width: size as u32,
+            height: size as u32
+        }
+    }
 }
 
 impl fmt::Display for Rect {
@@ -67,14 +87,19 @@ impl fmt::Display for Rect {
     }
 }
 
-fn area(rect: &Rect) -> u64 {
+fn area_ext(rect: &Rect) -> u64 {
     return (rect.width * rect.height) as u64;
 }
 
 fn area_runner() {
     let rect: Rect = Rect{width: 5, height: 10};
-    let area: u64 = area(&rect);
-    println!("{}, area: {}", rect, area);
+    let area_ext: u64 = area_ext(&rect);
+    let area: u64 = rect.area();
+    let square: Rect = Rect::square(3);
+    println!("{:?}", rect);
+    println!("{:#?}", rect);
+    println!("{}, area: {}, area_ext: {}, can_hold {}: {}",
+             rect, area, area_ext, square, rect.can_hold(&square));
 }
 
 fn main() {
