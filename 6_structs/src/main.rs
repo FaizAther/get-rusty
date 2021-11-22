3f6c82deffc0e77af0aaa5c300e9fc1c4d0060d9
FaizAther <faizather@protonmail.com>
2021-11-22 18:06:22 +1000

    structs

--- 6_structs/src/main.rs
+++ 6_structs/src/main.rs
@@ -1,5 +1,7 @@
 // Struct
 
+use std::fmt;
+
 struct User {
     id: u8,
     username: String,
@@ -8,22 +10,74 @@ struct User {
     active: bool    
 }
 
+//fn build_user(id: &mut u64, name: String, email: String) -> User {
+//    (*id) += 1;
+//    return User {
+//        id: (*id - 1) as u8,
+//        username: name,
+//        email: email,
+//        signin_count: 0,
+//        active: false
+//    };
+//}
+
+fn build_user_basic(id: &mut u64, name: String, email: String, basic: &User) -> User {
+    (*id) += 1;
+    return User {
+        id: (*id - 1) as u8,
+        username: name,
+        email: email,
+        ..(*basic)
+    };
+}
 fn show_user(user: &User) {
     println!("User:");
-    println!("\t{}", user.id);
-    println!("\t{}", user.username);
-    println!("\t{}", user.email);
-    println!("\t{}", user.signin_count);
-    println!("\t{}", user.active);
+    println!("|\t{}", user.id);
+    println!("|\t{}", user.username);
+    println!("|\t{}", user.email);
+    println!("|\t{}", user.signin_count);
+    println!("|\t{}", user.active);
+    println!("|_____");
+}
+
+fn user_runner() {
+    let mut id: u64 = 0;
+    let basic = User{
+        id: !(0) as u8,
+        username: String::from("basic"),
+        email: String::from("@jesus.com"),
+        signin_count: 0,
+        active: false
+    };
+//    id += 1;
+    show_user(&basic);
+    let u2 = build_user_basic(&mut id, String::from("jack"), String::from("jack@jesus.com"), &basic);
+    show_user(&u2);
+    println!("id: {}", id);
+}
+
+struct Rect {
+    width: u32,
+    height: u32
+}
+
+impl fmt::Display for Rect {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Rect: (width: {}, height: {})", self.width, self.height)
+    }
+}
+
+fn area(rect: &Rect) -> u64 {
+    return (rect.width * rect.height) as u64;
+}
+
+fn area_runner() {
+    let rect: Rect = Rect{width: 5, height: 10};
+    let area: u64 = area(&rect);
+    println!("{}, area: {}", rect, area);
 }
 
 fn main() {
-    let u1 = User{
-        id: 5,
-        username: String::from("John"),
-        email: String::from("john@jesus.com"),
-        signin_count: 2,
-        active: false
-    };
-    show_user(&u1);
+    user_runner();
+    area_runner();
 }
