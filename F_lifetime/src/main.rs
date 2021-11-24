10d53b857e5f0941d09013bba033277d28f65592
FaizAther <faizather@protonmail.com>
2021-11-24 16:03:35 +1000

    lifetimes

---
+++ F_lifetime/src/main.rs
@@ -0,0 +1,61 @@
+// Life
+
+// Concept 1
+
+fn life1() {
+	let _r: &i32;
+
+	{
+		let _x: i32 = 5;
+		_r = &_x;
+	}
+    //println!("r: {}", r); not allowed x has died
+}
+
+// Concept 2
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x }
+    else { y }
+}
+
+fn life2() {
+    let st1 = String::from("abc");
+    let res;
+
+    //{
+        let st2 = String::from("xy");
+        res = longest(st1.as_str(), st2.as_str());
+    //}
+
+    println!("{}", res);
+}
+
+// Concept 3
+#[derive(Debug)]
+struct ImpExt<'a> {
+    part: &'a str,
+}
+
+fn life3() {
+    let novel = String::from("Call me Ishmael. Some years ago... In the");
+    let sent_1 = novel.split('.').next().unwrap_or("Not found");
+    for ss in novel.split('.') {
+        println!("{}", ss);
+    }
+    let i = ImpExt {
+        part: sent_1
+    };
+    println!("{:?}", i);
+}
+
+fn life4() {
+    
+}
+
+fn main() {
+    life1();
+    life2();
+    life3();
+    life4();
+}
