5c5da8074d85cc86d2e9eb26ee3d0326cb5e2505
FaizAther <faizather@protonmail.com>
2021-11-26 18:29:53 +1000

    thing box

--- L_box_pointer/src/main.rs
+++ L_box_pointer/src/main.rs
@@ -1,5 +1,16 @@
 // Smart Pointers
 
+use std::fmt::Display;
+
+#[derive(Debug)]
+enum MyThing<T, U>
+    where T: PartialOrd,
+          U: Display
+{
+    NumericData(T),
+    TextData(U)
+}
+
 #[derive(Debug)]
 enum MyList<T> {
     Cons(T, Box<MyList<T>>),
@@ -14,4 +25,15 @@ fn main() {
     let list = Box::new(MyList::<i32>::Cons(44, nil));
     let list1 = Box::new(MyList::<i32>::Cons(55, list));
     println!("{:?}", list1);
+
+    let nill = Box::new(MyList::<MyThing<i32, &str>>::Nil);
+    let thing1 = MyThing::TextData("thing1");
+    let listt = Box::new(
+        MyList::<MyThing<i32, &str>>::Cons(thing1, nill)
+    );
+    let thing2 = MyThing::NumericData(44);
+    let listtt = Box::new(
+        MyList::<MyThing<i32, &str>>::Cons(thing2, listt)
+    );
+    println!("{:?}", listtt);
 }
