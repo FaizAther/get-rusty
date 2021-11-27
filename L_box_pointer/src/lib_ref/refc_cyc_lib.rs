174e00662b76c23988351ce876b480fa3d3fd13b
FaizAther <faizather@protonmail.com>
2021-11-28 01:45:55 +1000

    weak left

--- L_box_pointer/src/lib_ref/refc_cyc_lib.rs
+++ L_box_pointer/src/lib_ref/refc_cyc_lib.rs
@@ -75,24 +75,85 @@ impl<T> ListOps<T> for MyList<T>
 use std::ops::Deref;
 
 fn run1() {
+    println!("run1");
     let nil = MyList::<u64>::nil();
+
+    println!("\tl1 created");
     let l1 = MyList::<u64>::add(nil, 30003);
+    println!("\t{}: ref_count(l1)", Rc::strong_count(&l1.borrow()));
 
+    println!("\tl2 created");
     let l2 = MyList::<u64>::clone(10001, &l1.borrow());
+    println!("\t{}: ref_count(l1)", Rc::strong_count(&l2.borrow()));
+    println!("\t{}: ref_count(l2)", Rc::strong_count(&l1.borrow()));
 
-    println!("{:?}", MyList::tail(l1.borrow().deref()));
-    println!("{:?}", l1.borrow().deref().tail());
-    println!("{:?}", l1.borrow().deref().head());
-    println!("{:?}", MyList::head(l1.borrow().deref()));
-    println!("{:?}", l1);
+    println!("	{:?}", MyList::tail(l1.borrow().deref()));
+    println!("	{:?}", l1.borrow().deref().tail());
+    println!("	{:?}", l1.borrow().deref().head());
+    println!("	{:?}", MyList::head(l1.borrow().deref()));
+    println!("	{:?}", l1);
 
-    println!("{:?}", MyList::tail(l2.borrow().deref()));
-    println!("{:?}", l2.borrow().deref().tail());
-    println!("{:?}", l2.borrow().deref().head());
-    println!("{:?}", l2);
+    println!("	{:?}", MyList::tail(l2.borrow().deref()));
+    println!("	{:?}", l2.borrow().deref().tail());
+    println!("	{:?}", l2.borrow().deref().head());
+    println!("	{:?}", l2);
+}
+
+// The Leak Creator
+fn run2() {
+    println!("run2");
+    let nil = MyList::<u64>::nil();
+    println!("\tl1 created");
+    let l1 = MyList::<u64>::add(nil, 30003);
+    println!("\t{}: ref_count(l1)", Rc::strong_count(&l1.borrow()));
+
+    println!("\tl2 created");
+    let l2 = MyList::<u64>::clone(10001, &l1.borrow());
+    println!("\t{}: ref_count(l1)", Rc::strong_count(&l1.borrow()));
+    println!("\t{}: ref_count(l2)", Rc::strong_count(&l1.borrow()));
+
+    if let Some(link) = l1.borrow().tail() {
+        *link.borrow_mut() = Rc::clone(&l2.borrow());
+    };
+    println!("\tcycle created in l1 created");
+    println!("\t{}: ref_count(l1)", Rc::strong_count(&l1.borrow()));
+    println!("\t{}: ref_count(l2)", Rc::strong_count(&l1.borrow()));
+//
+//    println!("{:?}", MyList::tail(l1.borrow().deref()));
+//    println!("{:?}", l1.borrow().deref().tail());
+//    println!("{:?}", l1.borrow().deref().head());
+//    println!("{:?}", MyList::head(l1.borrow().deref()));
+//    println!("{:?}", l1);
+//
+//    println!("{:?}", MyList::tail(l2.borrow().deref()));
+//    println!("{:?}", l2.borrow().deref().tail());
+//    println!("{:?}", l2.borrow().deref().head());
+//    println!("{:?}", l2);
+}
+
+#[derive(Debug)]
+struct Node {
+    value:      u32,
+    children:   RefCell<Vec<Rc<Node>>>,
+}
+
+fn run3() {
+    let leaf = Rc::new(Node {
+        value:      3,
+        children:   RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value:      55,
+        children:   RefCell::new(vec![leaf.clone()]),
+    });
+
+    println!("{:?}", branch);
 }
 
 pub fn refc_cyc_lib_runner() {
     println!("refc_cyc_lib_runner");
     run1();
+    run2(); // this has a memory leak because of self reference
+    run3();
 } 
