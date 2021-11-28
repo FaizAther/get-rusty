43416b87d20164d5dff800bbdfdc3c6b3f924d0b
FaizAther <faizather@protonmail.com>
2021-11-28 19:54:00 +1000

    mutex

--- M_threads/src/lib_threads/lib_threads_1.rs
+++ M_threads/src/lib_threads/lib_threads_1.rs
@@ -51,12 +51,124 @@ fn check4() {
 
     //  v ownership moved inside thread closure
     let handle = thread::spawn(move || {
-        println!("{:?}",v); 
+        println!("\t{:?}",v); 
     });
 
     // println!("{:?}", v); not allowed
 
     handle.join().unwrap();
+}
+
+use std::sync::mpsc;
+
+fn passing1() {
+    println!("passing1");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msg = String::from("hi");
+        tx.send(msg).unwrap();
+    });
+
+    let recvd = rx.recv().unwrap();
+    println!("\t{}", recvd);
+}
+
+fn passing2() {
+    println!("passing2");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msgs = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("other"),
+            String::from("side"),
+        ];
+        tx.send(msgs).unwrap();
+    });
+
+    let recvd = rx.recv().unwrap();
+    println!("\t{:?}", recvd);
+    for msg in recvd {
+        println!("\t{}", msg);
+    }
+}
+
+fn passing3() {
+    println!("passing3");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msgs = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("other"),
+            String::from("side"),
+        ];
+        for msg in msgs {
+            tx.send(msg).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    while let Ok(recvd) = rx.recv() {
+        println!("\t{:?}", recvd);    
+    };
+    // for msg in recvd {
+    //     println!("\t{}", msg);
+    // }
+}
+
+fn passing4() {
+    println!("passing4");
+    let (tx, rx) = mpsc::channel();
+
+    let tx_copy = tx.clone();
+
+    thread::spawn(move || {
+        let msgs = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("other"),
+            String::from("side"),
+        ];
+        for msg in msgs {
+            tx.send(msg).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let msgs = vec![
+            String::from("I"),
+            String::from("must"),
+            String::from("have"),
+            String::from("called"),
+            String::from("a"),
+            String::from("thousand"),
+            String::from("times"),
+        ];
+        for msg in msgs {
+            tx_copy.send(msg).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    loop {
+        match rx.recv() {
+            Ok(recvd) => { 
+                println!("\tMsg={:?}", recvd);
+            }
+            Err(err) => {
+                println!("\tErr={:?}", err);
+                break;
+            }
+        }
+    }
 }
 
 pub fn lib_threads_1_runner() {
@@ -65,4 +177,8 @@ pub fn lib_threads_1_runner() {
     check2();
     check3();
     check4();
+    passing1();
+    passing2();
+    passing3();
+    passing4();
 }
